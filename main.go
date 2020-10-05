fd8c0b1ae11287eb35d72eecca46bf2d964283e5
morzik45 <morzik45@gmail.com>
2020-10-05 16:36:29 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "log"
+
+type Response struct {
+	StatusCode int `json:"statusCode"`
+}
+
+type EventMetadataStruct struct {
+	EventId string `json:"event_id"`
+	EventType string `json:"event_type"`
+	CreatedAt string `json:"created_at"`
+}
+
+type MessageStruct struct {
+	MessageID string `json:"message_id"`
+	MD5OfBody string `json:"md5_of_body"`
+	Body string `json:"body"`
+}
+
+type DetailsStruct struct {
+	QueueId string `json:"queue_id"`
+	Message MessageStruct `json:"message"`
+}
+
+type Message struct {
+	EventMetadata EventMetadataStruct `json:"event_metadata"`
+	Details DetailsStruct `json:"details"`
+}
+
+func Handler(messages []Message) (*Response, error) {
+	log.Println(messages[0].Details.Message.Body)
+	return &Response{StatusCode: 200}, nil
+}
