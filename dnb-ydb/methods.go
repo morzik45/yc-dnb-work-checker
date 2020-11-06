fe92c6356e8048ead441a8003b71c19aa3da658e
morzik <morzik45@gmail.com>
2020-11-06 21:42:55 +0500

    add db

--- dnb-ydb/methods.go
+++ dnb-ydb/methods.go
@@ -47,7 +47,7 @@ func (db DB) InsertWork(userID uint64, status uint8) error {
 	return err
 }
 
-func (db DB) CountFreeWorkFromCurrentDay(userID uint64) error {
+func (db DB) CountFreeWorkFromCurrentDay(userID uint64) (uint64, error) {
 	readTx := table.TxControl(
 		table.BeginTx(
 			table.WithSerializableReadWrite(),
@@ -58,18 +58,28 @@ func (db DB) CountFreeWorkFromCurrentDay(userID uint64) error {
 	query := fmt.Sprintf(`
 		PRAGMA TablePathPrefix("%s");
 		DECLARE $user_id AS Uint64;
-		SELECT COUNT(*) FROM works WHERE user_id = $user_id AND time > `, Database)
+		$time = DateTime::MakeTimestamp(CurrentUtcDate());
+		SELECT
+			COUNT_IF(user_id = $user_id AND status = 0 AND time > $time) AS count FROM works;`, Database)
+
+	var count uint64
 
 	err := table.Retry(db.Ctx, db.SessionPool,
 		table.OperationFunc(func(ctx context.Context, s *table.Session) (err error) {
-			_, _, err = s.Execute(ctx,
+			_, res, err := s.Execute(ctx,
 				readTx,
 				query,
 				table.NewQueryParameters(
 					table.ValueParam("$user_id", ydb.Uint64Value(userID)),
 				),
 			)
-			return
+			if err != nil || res.Err() != nil {
+				return err
+			}
+			if res.NextSet() && res.NextRow() && res.NextItem() {
+				count = res.Uint64()
+			}
+			return err
 		}))
-	return err
+	return count, err
 }
--- modelWork.go
+++ modelWork.go
@@ -51,6 +51,12 @@ func (w *Work) AddNewWorkToQueue() error {
 }
 
 func (w *Work) SetStatus() error {
+
+	ydbc := make(chan *dnb_ydb.DB)
+	go func(c chan *dnb_ydb.DB) {
+		c <- dnb_ydb.NewDB()
+	}(ydbc)
+
 	w.WorkType = 0    // Работа из TG
 	w.WorkStatus = -1 // Изначально отказ
 	var ctx = context.Background()
@@ -60,21 +66,14 @@ func (w *Work) SetStatus() error {
 	if err != nil {
 		return err
 	}
-	wdb, err := dnb_mongo.NewStatisticDB(ctx)
-	if err != nil {
-		return err
-	}
-
-	ydbc := make(chan *dnb_ydb.DB)
-
-	go func(c chan *dnb_ydb.DB) {
-		c <- dnb_ydb.NewDB()
-	}(ydbc)
 
 	currentUser, err := db.GetUser(ctx)
 	if err != nil {
 		return err
 	}
+
+	yaDB := <-ydbc
+	defer yaDB.Close()
 
 	if currentUser.Status.IsAdmin { // Если админ
 		w.WorkStatus = 3
@@ -122,30 +121,12 @@ func (w *Work) SetStatus() error {
 		w.WorkStatus = 1
 
 	} else { // Бесплатная
-		t := time.Now().UTC()
-		rounded := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
-		c, err := wdb.CountDocuments(ctx, bson.D{
-			primitive.E{
-				Key:   "user_id",
-				Value: strconv.Itoa(w.UID),
-			},
-			primitive.E{
-				Key:   "status",
-				Value: 0,
-			},
 
-			primitive.E{
-				Key: "time",
-				Value: bson.D{
-					{
-						"$gt",
-						rounded,
-					}},
-			},
-		})
+		c, err := yaDB.CountFreeWorkFromCurrentDay(uint64(w.UID))
 		if err != nil {
 			return err
 		}
+
 		if c < 3 {
 			w.WorkStatus = 0
 			update := bson.D{
@@ -165,12 +146,6 @@ func (w *Work) SetStatus() error {
 	}
 
 	if w.WorkStatus != -1 {
-		err = wdb.InsertWork(ctx, w.WorkStatus)
-		if err != nil {
-			return err
-		}
-		yaDB := <-ydbc
-		defer yaDB.Close()
 
 		err = yaDB.InsertWork(uint64(w.UID), uint8(w.WorkStatus))
 		if err != nil {
